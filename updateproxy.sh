5749a88bcfd419c1fbae30217ea3a780ea3a2425
Matija Sirk <sirkmatija@gmail.com>
2016-08-04 00:21:27 +0200

    Update updateproxy.sh

--- updateproxy.sh
+++ updateproxy.sh
@@ -3,12 +3,12 @@
 #Change proxy every DELAY seconds for MAX times
 FILE=./toriptables2.py
 COUNTER=0
-MAX=$1 #
+MAX=$1
 DELAY=$2
 
 #Check if arg was received
 if [ -z "$2" ]; then 
-	echo "USAGE: sudo ./updateproxy.sh <number-of-repetitions> <delay-between-repetitions>"
+	echo "USAGE: sudo ${0} <number-of-repetitions> <delay-between-repetitions>"
 	exit
 fi
 
